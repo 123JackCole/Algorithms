dc390b6539e9c468449419d2eda452a9210f6e48
123JackCole <123jack.cole@gmail.com>
2020-11-21 21:00:25 -0800

    added a leetcode problem and created the folder

---
+++ leetcode1656.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/contest/weekly-contest-215/problems/design-an-ordered-stream/
+
+
+
+
+/**
+ * @param {number} n
+ */
+var OrderedStream = function(n) {
+    
+    
+    
+};
+
+/** 
+ * @param {number} id 
+ * @param {string} value
+ * @return {string[]}
+ */
+
+OrderedStream.prototype.insert = function(id, value) {
+    
+    
+    
+};
+
+/** 
+ * Your OrderedStream object will be instantiated and called as such:
+ * var obj = new OrderedStream(n)
+ * var param_1 = obj.insert(id,value)
+ */
