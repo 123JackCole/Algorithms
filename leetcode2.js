0b95056727bc8950d79809103c858fbebcb7ea44
123JackCole <123jack.cole@gmail.com>
2020-12-17 22:53:24 -0800

    commit

--- leetcode2.js
+++ leetcode2.js
@@ -52,6 +52,7 @@ const addTwoNumbers = (l1, l2) => {
     console.log(sum)
 
     const ll = [];
+    
     // while (sum % 10 && sum %100) {
     //     ll.push(ListNode(sum % 10, sum % 100));
     //     sum /= 10;
