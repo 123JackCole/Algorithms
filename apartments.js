b1930741634a3ee46a8499ac28ed68a6c4c15073
123JackCole <123jack.cole@gmail.com>
2021-05-06 10:16:53 -0700

    added practice problem from practice interview

---
+++ apartments.js
@@ -0,0 +1,18 @@
+const blocks = [
+  { gym: false, school: true, store: false },
+  { gym: true, school: false, store: false },
+  { gym: true, school: true, store: false },
+  { gym: false, school: false, store: false },
+  { gym: false, school: true, store: true },
+  { gym: false, school: false, store: false },
+];
+
+const reqs = ['gym', 'school', 'store'];
+
+const findApartment = (blocks, reqs) => {
+
+    
+
+}
+
+console.log(findApartment(blocks, reqs));
