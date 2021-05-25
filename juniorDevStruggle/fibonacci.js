38321d600a4856dbf9f686b2b8b65bcf321b498f
123JackCole <123jack.cole@gmail.com>
2021-05-24 20:32:17 -0700

    problems from meetup

---
+++ juniorDevStruggle/fibonacci.js
@@ -0,0 +1,19 @@
+// write a function that takes n and returns the first n items in the fibonacci sequence
+// 1 1 2 3 5 8 13 21 34
+
+// return an array
+// 
+
+const fibonacci = (n) => {
+
+    const nums = [];
+
+    for (let i = 0; i < n; i++) {
+
+
+
+    }
+
+    return nums;
+
+}
---
+++ juniorDevStruggle/stringProblem1.js
@@ -0,0 +1,26 @@
+// implement a search method for a binary tree
+
+//                          5
+//              1                         8
+//                      2          6                9
+
+// node contains: val, left, right
+// boolean
+
+// recursive solution
+// pass in a node
+
+// check val
+// return true if match
+
+// check if left val is less than current val
+
+
+
+
+// 2 dimensiona app problem
+// given a 2 dimensional grid
+// find path from top left to bottom right
+// can only move right and down
+// return if there is a path
+
