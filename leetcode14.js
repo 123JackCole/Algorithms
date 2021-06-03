c732f7eaceacb1850c6e3d736d3949427820d46b
123JackCole <123jack.cole@gmail.com>
2021-06-03 15:09:12 -0700

    added problem and solution

---
+++ leetcode14.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ const longestCommonPrefix = (strs) => {
+    
+    let prefix = strs[0],
+        temp = "",
+        curr;
+    
+    for (let i = 1; i < strs.length; i++) {
+        curr = strs[i];
+        
+        for (let j = 0; j < curr.length; j++) {
+            
+            if (prefix[j] === curr[j]) {
+                temp += curr[j];
+            } else {
+                break;
+            }
+            
+        }
+        prefix = temp;
+        temp = "";
+        
+    }
+    
+    return prefix;
+    
+};
