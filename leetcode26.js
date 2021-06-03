828bcc733336fae3ff9af1e432be91709832c59a
123JackCole <123jack.cole@gmail.com>
2021-06-03 16:09:55 -0700

    added problem and solution

---
+++ leetcode26.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ const removeDuplicates = (nums) => {
+    
+    for (let i = 0; i < nums.length; i++) {
+        
+        if (nums[i] === nums[i + 1]) {
+            
+            nums.splice(i, 1);
+            i--;
+            
+        }
+        
+    }
+    
+};
