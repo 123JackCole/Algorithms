02aca41fd09d3e1cdac1e8fd5a0d4be335cb684f
123JackCole <123jack.cole@gmail.com>
2021-06-03 12:01:24 -0700

    added in first solution

---
+++ leetcode6.js
@@ -0,0 +1,41 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+ const convert = (s, numRows) => {
+    
+    const map = {}
+    for (let i = 0; i < numRows; i++) {
+        map[i] = ""
+    }
+    
+    let counter = 0;
+    
+    while(counter < s.length) {
+        // add in straight line down
+        for (let i = 0; i < numRows; i++) {
+            map[i] += s[counter];
+            counter++;
+            if (counter === s.length) break;
+        }
+        if (counter === s.length) break;
+        // add in diagonal line up
+        for (let i = numRows - 2; i > 0; i--) {
+            map[i] += s[counter];
+            counter++;
+            if (counter === s.length) break;
+        }
+    }
+    
+    // merge the strings together
+    
+    let finalString = "";
+    
+    for (let i = 0; i < numRows; i++) {
+        finalString += map[i];
+    }
+    
+    return finalString;
+    
+};
