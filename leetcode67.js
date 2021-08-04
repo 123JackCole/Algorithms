c37b11a4f149133155ef287710e0d4f77c0b8112
123JackCole <123jack.cole@gmail.com>
2021-08-04 16:15:07 -0700

    current work on the problem, not done

---
+++ leetcode67.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+ const addBinary = (a, b) => {
+    
+    lengthA = a.length;
+    lengthB = b.length;
+  
+    const longestLength = Math.max(lengthA, lengthB);
+
+    let counter = 0,
+        binarySum = "",
+        currSum;
+    
+    while (counter < longestLength) {
+        
+        
+        
+    }
+    
+};
